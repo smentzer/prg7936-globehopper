73dd3c0fde892e101187b362c9f6d0bf68bfea50
Cayde6 <stephaniementzer@gmail.com>
2022-04-25 20:55:34 -0400

    initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,20 @@
 import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
+import login from './Pages/login';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3719bf",
+      light: "#441fed",
+      dark: "#2c1596"
+    },
+  }
+});
+
 
 function App() {
   const [testData, setTestData] = useState(null);
@@ -12,25 +26,9 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {testData ? (
-          <pre>{testData}</pre>
-        ) : null}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    <login/>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/Pages/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function login() {
+ <div>
+     <h1>LOG IN</h1>
+ </div>
+}
+
+
+export default login;
